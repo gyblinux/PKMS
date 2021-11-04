c4c1cbb53dd13bf41d8860bb63afd4583095ef40
CC Sapphire <gybsapphire@gmail.com>
2021-11-04 21:32:10 +0000

    post and para model set and tested

--- app/models.py
+++ app/models.py
@@ -1,11 +1,91 @@
 from django.db import models
-from django.db.models.fields import AutoField
+from django.db.models.fields import AutoField, DateField
 
 # Create your models here.
 class Para(models.Model):
     para_id = models.AutoField(primary_key=True);
     content = models.TextField();
-    previous = models.OneToOneField('self', on_delete=models.SET_NULL, null=True, blank=True, related_name="next");
+    previous = models.OneToOneField('self', on_delete=models.SET_NULL, null=True, default=None, related_name="next");
+
+    def is_end(self) -> bool:
+        """checking if this paragraph is the last para in the linkedlist"""
+        return not hasattr(self, "next");
+
+    def is_linked(self) -> bool:
+        """checking if this paragraph is appended to the linkedlist"""
+        return self.previous is not None;
+
+    def link_to(self, predecessor: "Para" = None, post: "Post" = None) -> None:
+        """append OR insert this para to another para, its predecessor"""
+        # early return: must name either predecessor para or header post
+        if (predecessor is None) and (post is None):
+            raise Exception("Must name either a para or a post to complete the insertion");
+        if (predecessor is not None) and (post is not None):
+            raise Exception("Insertion Error: cannot insert the para to two different position");
+
+        # to a paragraph
+        if (predecessor is not None) and (post is None):
+            # append to SLL
+            if predecessor.is_end():
+                self.previous = predecessor;
+                self.save();
+            # insert to SLL
+            else:
+                predecessor.next.previous = self;
+                predecessor.next.save();
+                self.previous = predecessor;
+                self.save();
+        # to a post header
+        elif (predecessor is None) and (post is not None):
+            if post.index_para is not None:
+                post.index_para.previous = self;
+                post.index_para.save();
+            post.set_index_para(self);
+    
+    def delete_myself(self) -> None:
+        """destruct this para and repair the SLL connections if there are any"""
+        # delete from tail
+        if self.is_end():
+            self.delete();
+        # delete with successor
+        else:
+            if hasattr(self, "of_post"):
+                self.of_post.set_index_para(self.next);
+            else:
+                prev = self.previous;
+                temp = self.next;
+                self.delete();
+                temp.previous = prev;
+                temp.save();
 
     def __str__(self) -> str:
         return self.content;
+
+class Post(models.Model):
+    # PK
+    post_id = models.AutoField(primary_key=True);
+    # props
+    is_shared = models.BooleanField(default=False);
+    last_modified = models.DateTimeField(auto_now=True);
+    post_title = models.CharField(max_length=50, default="Untitled");
+    # header of SLL(single linked list)
+    index_para = models.OneToOneField(Para, on_delete=models.SET_DEFAULT, null=True, default=None, related_name="of_post");
+
+    def set_index_para(self, para: "Para") -> None:
+        self.index_para = para;
+        self.save();
+
+    def render_post(self) -> str:
+        """render the entire post with linked-list paragraphs if there exsits any"""
+        probe = self.index_para;
+
+        # early return with empty post
+        if (probe is None): return "There are no contents in this post yet";
+        
+        # render all paragraphs with non-empty post
+        content = ""
+        while True:
+            content += "%s\n" % probe;
+            if not probe.is_end(): probe = probe.next;
+            else: break;
+        return content;
--- app/tests.py
+++ app/tests.py
@@ -1,13 +1,82 @@
 from django.test import TestCase
 from app.models import Para
+from app.models import Post
 # Create your tests here.
 class ModelTest(TestCase):
-    def test_linkedlist(self):
-        p1 = Para.objects.create(content="I'm the first paragraph");
-        p2 = Para.objects.create(content="This is the second paragraph");
-        p2.previous = p1;
-        p2.save();
+    def setUp(self) -> None:
+        Para.objects.create(content="I'm the first paragraph");
+        Para.objects.create(content="This is the SECOND para");
+        Para.objects.create(content="The third paragraph");
+        Post.objects.create();
+
+    def test_append_to_paragraph(self):
+        p1 = Para.objects.get(pk=1);
+        p2 = Para.objects.get(pk=2);
+        p2.link_to(p1);
+        self.assertEqual(p2.previous, p1);
+        self.assertTrue(hasattr(p1, "next"));
+        self.assertEqual(p1.next, p2);
+    
+    def test_insert_to_paragraph(self):
+        p1 = Para.objects.get(pk=1);
+        p2 = Para.objects.get(pk=2);
+        p3 = Para.objects.get(pk=3);
+        p3.link_to(predecessor=p1);
+        p2.link_to(predecessor=p1);
+        self.assertTrue(hasattr(p1, "next"));
         self.assertEqual(p1.next, p2);
         self.assertEqual(p2.previous, p1);
+        self.assertEqual(p2.next, p3);
+        self.assertEqual(p3.previous, p2);
+
+    def test_append_to_post_header(self):
+        post = Post.objects.get(pk=1);
+        p1 = Para.objects.get(pk=1);
+        p1.link_to(post=post);
+        self.assertTrue(hasattr(p1, "of_post"));
+        self.assertIsNotNone(post.index_para);
+        self.assertTrue(post.index_para, p1);
+        self.assertTrue(p1.of_post, post);
+    
+    def test_insert_to_post_header(self):
+        post = Post.objects.get(pk=1);
+        p1 = Para.objects.get(pk=1);
+        p2 = Para.objects.get(pk=2);
+        p1.link_to(post=post);
+        p2.link_to(post=post); # should result in post-p1-p2;
+        self.assertEqual(post.index_para, p2);
+        self.assertEqual(p2.next, p1);
+        self.assertTrue(p1.is_end());
+    
+    def test_delete_para(self):
+        post = Post.objects.get(pk=1);
+        p1 = Para.objects.get(pk=1);
+        p1.link_to(post=post);
+        self.assertTrue(p1.is_end());
+        p1.delete();
+        post = Post.objects.get(pk=1); # must re-query to get the right reference of object
+        self.assertIsNone(post.index_para);
         
-        
+    def test_delete_para_after_header(self):
+        post = Post.objects.get(pk=1);
+        p1 = Para.objects.get(pk=1);
+        p1.link_to(post=post);
+        p2 = Para.objects.get(pk=2);
+        p2.link_to(predecessor=p1);
+        p1.delete_myself();
+        post = Post.objects.get(pk=1);
+        self.assertEqual(post.index_para, p2);
+    
+    def test_delete_middle_para(self):
+        post = Post.objects.get(pk=1);
+        p1 = Para.objects.get(pk=1);
+        p1.link_to(post=post);
+        p2 = Para.objects.get(pk=2);
+        p2.link_to(predecessor=p1);
+        p3 = Para.objects.get(pk=3);
+        p3.link_to(predecessor=p2);
+        p2.delete_myself();
+        p1 = Para.objects.get(pk=1);
+        p3 = Para.objects.get(pk=3);
+        self.assertEqual(p1.next, p3);
+        self.assertEqual(p3.previous, p1);
